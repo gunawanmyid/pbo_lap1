a7ce49ec0f87e18b86e221463072d36c692b2943
Fadhli12 <thegenesispath@gmail.com>
2015-04-21 18:51:02 +0800

    Create hitung.java

---
+++ hitung.java
@@ -0,0 +1,25 @@
+package laporan_1;
+
+public class hitung {
+	
+	static int c;
+	static float d;
+	
+	int tambah(int a, int b){
+		c=a+b;
+		return c;
+	}
+	int kurang(int a, int b){
+		c=a-b;
+		return c;
+	}
+	float kali(float a, float b){
+		d=a*b;
+		return d;
+	}
+	float bagi(float a, float b){
+		d=a/b;
+		return d;
+	}
+	
+}
